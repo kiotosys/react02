6013aa84ba7c31e34e61c00a4a3b1409d6a62112
Master nn <master@gmail.com>
2023-10-25 19:22:28 -0500

    v7. Validacion, Lectura a BD

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 
+import { useState } from 'react';
 import AppForm from './components/AppForm';
 import logo from './logo.svg';
 //import './App.css';
 import C01componente from './pagina/C01componente';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { db } from './components/firebase';
 
 function App() {
+  ////// READ - LECTURA - fnRead //////////
+  const [docBD, setDocBD] = useState([]);
+  const fnRead = () => {
+    try {
+      const xColeccionConQuery = query(collection(db, 'persona'));
+      const unsubscribe = onSnapshot(xColeccionConQuery, (xDatosBD) => {
+        const xDoc = [];
+        xDatosBD.forEach((doc) => {
+          xDoc.push({id:doc.id, ...doc.data()});
+        });
+        setDocBD(xDoc);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fnRead(); //Prueba sin useEffect;
+
+  ////// DELETE - Eliminar - fnDelete /////
+  const [idActual, setIdActual] = useState("");
+  const fnDelete = (xId) => {
+  }
+
   return (
     <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
-      <AppForm />
-      <p>No. 1 Juan manuel       x   A</p>
-      <p>No. 1 Rosa Maria        x   A</p>
+      <AppForm {...{idActual, setIdActual, fnRead}} />
+      {
+        docBD.map((p) => 
+          <p key={p.id}> 
+            No. {p.nombre}...
+            <span onClick={() => fnDelete(p.id)}>x</span> 
+            .....
+            <span onClick={() => setIdActual(p.id)}>A</span>
+          </p>
+        )
+      }
     </div>
   );
 }
--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -9,31 +9,59 @@ const AppForm = () => {
   ///////////// GUARDAR / ACTUALIZAR ////////
   const camposRegistro = { nombre:"", edad:"", genero:""}
   const [objeto, setObjeto] = useState(camposRegistro);
+  console.log(objeto);
 
   const handleSubmit = (e) => {   //manejador de submit
     e.preventDefault();
     try {
-      if(db){
+
+      if(validarForm()){
         addDoc(collection(db, 'persona'), objeto);
         console.log("Se guardo con exito");
+      }else{
+        console.log("No se guardo");
       }
     } catch (error) {
       console.error();
     }
   }
 
+  //Manejedor del estado de cambios
+  const handleStatusChange = (e) => {
+    const {name, value} = e.target;
+    setObjeto({...objeto, [name]: value});
+    //console.log({name, value});
+  }
+
+  // Validación
+  const validarForm = () => {
+    if(objeto.nombre === ""){
+      alert("Escriba nombre...");
+      return false;
+    }
+
+    return true;
+  }
 
   return (
     <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
       <form onSubmit={handleSubmit}>
         <h1>AppForm.js</h1>
-        <input type='text' placeholder='Nombre...' />
-        <input type='text' placeholder='Nombre...' />
-        <select>
+
+        <input onChange={handleStatusChange} 
+         value={objeto.nombre} name='nombre' 
+         type='text' placeholder='Nombre...' /> <br/>
+
+        <input onChange={handleStatusChange} 
+         value={objeto.edad} name='edad' 
+         type='text' placeholder='Edad...' /> <br/>
+
+        <select onChange={handleStatusChange} 
+          name='genero' > 
           <option value="">Seleccione genero...</option>
           <option value="M"> Masculino </option>
           <option value="F"> Femenino </option>
-        </select>
+        </select> <br />
         <button>
           Guardar / Actualizar
         </button>
